1f63746b445689fcd6b6a18ef5d11828eee9c735
overexposer <overexposer@yahoo.com>
2017-12-06 10:15:23 -0600

    Update L298N_MotorDriver.cpp

--- src/L298N_MotorDriver.cpp
+++ src/L298N_MotorDriver.cpp
@@ -1,70 +1,89 @@
+#pragma once
+
 #include "Arduino.h"
 #include "L298N_MotorDriver.h"
 
-void L298N::setSpeed(uint8_t speed) 
+L298N_MotorDriver::L298N_MotorDriver() {}
+
+MotorDriver *L298N_MotorDriver::getMotor(uint8_t num)
 {
-  _speed = speed;
-  return;
+  if (num > 2) return NULL;
+
+  num--;
+
+  if (dcmotors[num].motornum == 0) {
+    // not init'd yet!
+    dcmotors[num].motornum = num;
+    uint8_t pwm;
+    if (num == 0) {
+      pwm = 5;
+    }
+    else if (num == 1) {
+      pwm = 6;
+    }
+    dcmotors[num].PWMpin = pwm;
+  }
+  return &dcmotors[num];
 }
 
-uint8_t L298N::getSpeed()
+/******************************************
+               MOTORS
+******************************************/
+
+MotorDriver::MotorDriver()
 {
-  return _speed;
+  motornum = 0;
+  PWMpin = 0;
 }
-void L298N::run(uint8_t cmd) 
+
+void MotorDriver::run(uint8_t cmd)
 {
-  // pins 3 and 4 are used for drive motor output
-  const uint8_t EnableDrivePin = 5; // pwm pin for drive motor
-  _EnableDrive = EnableDrivePin;
+//  uint8_t EnableDrivePin = 5;
+//  _Enablepin = EnableDrivePin;
   
   switch (cmd) {
   case FORWARD:
     CLR(PORTD, 4); // LOW
     SET(PORTD, 3); // HIGH  
-    analogWrite(_EnableDrive, _speed);
-  break;
+    analogWrite(PWMpin, _pwmVal);
+    break;
   case BACKWARD:
     SET(PORTD, 4); // HIGH
     CLR(PORTD, 3); // LOW
-    analogWrite(_EnableDrive, _speed);
-  break;
-  case RELEASE:
-    CLR(PORTD, 4); // LOW
-    CLR(PORTD, 3); // LOW
-    analogWrite(_EnableDrive, _speed);
-  break;
+    analogWrite(PWMpin, _pwmVal);
+    break;
   case BRAKE:
     SET(PORTD, 4); // HIGH
     SET(PORTD, 3); // HIGH
-    analogWrite(_EnableDrive, _speed);
-  break;
+    analogWrite(PWMpin, _pwmVal);
+    break;
+  case RELEASE:
+    CLR(PORTD, 4); // LOW
+    CLR(PORTD, 3); // LOW
+    analogWrite(PWMpin, _pwmVal);
+    break;
+  case LEFT:
+    SET(PORTC, 0); // HIGH
+    CLR(PORTC, 1); // LOW
+    analogWrite(PWMpin, _pwmVal);
+    break;
+  case RIGHT:
+    CLR(PORTC, 0); // LOW
+    SET(PORTC, 1); // HIGH
+    analogWrite(PWMpin, _pwmVal);
+    break;
+  case STRAIGHT:
+    CLR(PORTC, 0); // LOW
+    CLR(PORTC, 1); // LOW
+    analogWrite(PWMpin, _pwmVal);
+    break;
   }
 }
 
-void L298N::steer(uint8_t cmd) 
+void MotorDriver::setSpeed(uint8_t pwmVal)
 {
-  // pins 14 and 15 are used for steering motor output
-  const uint8_t EnableTurnPin = 6;
-  _EnableTurn = EnableTurnPin;
-  
-  switch (cmd) {
-  case 5:
-    // left
-    SET(PORTC, 0); // HIGH
-    CLR(PORTC, 1); // LOW
-    analogWrite(_EnableTurn, _speed);
-  break;
-  case 6:
-    // right
-    CLR(PORTC, 0); // LOW
-    SET(PORTC, 1); // HIGH
-    analogWrite(_EnableTurn, _speed);
-  break;
-  case 7:
-    CLR(PORTC, 0); // LOW
-    CLR(PORTC, 1); // LOW
-    analogWrite(_EnableTurn, _speed);
-  break;
-  }
+  _pwmVal = pwmVal;
+  return;
 }
+  
 
